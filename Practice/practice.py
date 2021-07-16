ed1f9953eb911e27e7d8bce6e4e5084b616c7b85
Rockwell70 <rockwellshabani@gmail.com>
2021-07-16 11:05:04 -0700

    streamlit work

--- Practice/practice.py
+++ Practice/practice.py
@@ -4,9 +4,9 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"])
+# st.write(
+#     "Has environment variables been set:",
+#     os.environ["db_username"] == st.secrets["db_username"])
 
 df = pd.read_excel('EE_Data.xlsx', usecols='P,K')
 summary = pd.pivot_table(df, index='MSA', values='Annual Salary', aggfunc=np.mean)
