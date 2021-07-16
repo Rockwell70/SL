f1535e5d23c0b68bb543fc5834569f97cec968cc
Rockwell70 <rockwellshabani@gmail.com>
2021-07-16 10:14:32 -0700

    streamlit work

---
+++ practice.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+df = pd.read_excel('/Users/rockwell/SH/Work/EEData/EE_Data.xlsx', usecols='P,K')
+summary = pd.pivot_table(df, index='MSA', values='Annual Salary', aggfunc=np.mean)
+st.dataframe(summary)
