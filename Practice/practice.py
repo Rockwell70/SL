6cbd03f4bcd97e26300ccc6e26ae7e88e55557e1
Rockwell70 <rockwellshabani@gmail.com>
2022-05-17 13:11:06 -0700

    streamlit work

--- Practice/practice.py
+++ Practice/practice.py
@@ -4,9 +4,33 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-# st.write(
-#     "Has environment variables been set:",
-#     os.environ["db_username"] == st.secrets["db_username"])
+pd.options.display.float_format = "{:0,.2f}".format
 
-df = pd.read_excel('Pay Range Master 051722.xlsb', sheet_name='Master_List')
-st.dataframe(df)
+st.set_page_config(
+     page_title="Job Title list",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded"
+ )
+
+df = pd.read_excel('Pay Range Master 051722.xlsb', sheet_name='Master_List', converters={"Grade":str,
+                                                                                         "AIP %":str,
+                                                                                         'Job Code':int})
+df['Hourly Min'] = round(df['Hourly Min'],2)
+df['Hourly Mid'] = round(df['Hourly Mid'],2)
+df['Hourly Max'] = round(df['Hourly Max'],2)
+
+df['Annual Min'] = round(df['Annual Min'],0)
+df['Annual Mid'] = round(df['Annual Mid'],0)
+df['Annual Max'] = round(df['Annual Max'],0)
+
+
+titles = ['']
+
+for title in df['Job Title'].drop_duplicates():
+    titles.append(title)
+
+title_choice = st.sidebar.selectbox("Select a title:", titles)
+if title_choice != '':
+    df = df[df['Job Title'] == title_choice]
+st.dataframe(df.style.format(subset=['Annual Min', 'Annual Mid','Annual Max'], formatter="{:,.0f}"))
